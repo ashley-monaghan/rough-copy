1f1a667d30d42dffd115f1c42cc98caf390375f3
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2023-01-10 21:15:43 -0500

    rough-copy

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
+  navigator.serviceWorker.register("/rough-copy/sw.js", {
+    scope: "/rough-copy/",
   })
 }
 
