1b1b3b9301ae6d069cc1cce705ccda47f9c4eff8
Ash! <113056162+ashley-monaghan@users.noreply.github.com>
2023-01-14 14:12:12 -0500

    ||

--- js/script.js
+++ js/script.js
@@ -62,8 +62,8 @@ function crystalButton() {
   } else if (inputedMonth == "December"){
     document.getElementById("crystalInfo").innerHTML =
     "Turquoise"
-  } else if ((inputedMonth == "Month") && (inputedDay == "Day" )) {
-    document.getElementById("zodiacInfo").innerHTML =
+  } else if ((inputedMonth == "Month") || (inputedDay == "Day" )) {
+    document.getElementById("crystalInfo").innerHTML =
     "Please select your birthday in the selection boxes above..."
   } else {
     document.getElementById("zodiacInfo").innerHTML =
@@ -118,7 +118,7 @@ function zodiacButton() {
   } else if (((inputedMonth == "December") && (inputedDay == "22" || inputedDay == "23" || inputedDay == "24" || inputedDay == "25" || inputedDay == "26" || inputedDay == "27" || inputedDay == "28" || inputedDay == "29" || inputedDay == "30" || inputedDay == "31")) || ((inputedMonth == "January") && (inputedDay == "1" || inputedDay == "2" || inputedDay == "3" || inputedDay == "4" || inputedDay == "5" || inputedDay == "6" || inputedDay == "7" || inputedDay == "8" || inputedDay == "9" || inputedDay == "10" || inputedDay == "11" || inputedDay == "12" || inputedDay == "13" || inputedDay == "14" || inputedDay == "15" || inputedDay == "16" || inputedDay == "17" || inputedDay == "18" || inputedDay == "19"))) {
     document.getElementById("zodiacInfo").innerHTML =
       "Capricorn"
-  } else if ((inputedMonth == "Month") && (inputedDay == "Day" )) {
+  } else if ((inputedMonth == "Month") || (inputedDay == "Day" )) {
     document.getElementById("zodiacInfo").innerHTML =
     "Please select your birthday in the selection boxes above..."
   } else {
